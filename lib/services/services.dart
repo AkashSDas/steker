c10d0026a82b2be1dc9e5581a4aadd57a17f4db0
Akash Das <aakashdas368@gmail.com>
2020-11-28 23:53:53 +0530

    add AuthService to handle authentication

---
+++ lib/services/services.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  // Firebase user one-time fetch
+  User get getUser => _auth.currentUser;
+
+  // Firebase user a realtime stream
+  Stream<User> get user => _auth.authStateChanges();
+
+  Future<User> anonLogin() async {
+    UserCredential credential = await _auth.signInAnonymously();
+    print(credential);
+  }
+}
