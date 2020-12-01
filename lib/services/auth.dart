59fbcfbf4d8ac6e3333a657a6bb864bc2dc200a0
Akash Das <aakashdas368@gmail.com>
2020-12-01 18:33:55 +0530

    add google auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -15,10 +15,33 @@ class AuthService {
   // Firebase user a realtime stream
   Stream<User> get user => _auth.authStateChanges();
 
+  // Anonymous Firebase login
   Future<User> anonLogin() async {
     try {
       UserCredential credential = await _auth.signInAnonymously();
+
       return credential.user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  // Sign in with Google
+  Future<User> googleSignIn() async {
+    try {
+      GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleSignInAuth =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuth.accessToken,
+        idToken: googleSignInAuth.idToken,
+      );
+
+      UserCredential result = await _auth.signInWithCredential(credential);
+      User user = result.user;
+
+      return user;
     } catch (e) {
       return null;
     }
