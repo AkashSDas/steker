2b1a7f5b97b12628a90f6597739bc0eee1191280
Akash Das <aakashdas368@gmail.com>
2020-12-02 21:01:18 +0530

    get stickers collection data in HomeScreen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:steker/services/globals.dart';
+import 'package:steker/services/models.dart';
 import 'package:steker/shared/drawer.dart';
+import 'package:steker/shared/loader.dart';
 import 'package:steker/shared/shared.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -27,7 +30,30 @@ class HomeScreen extends StatelessWidget {
                 title: 'home',
                 currentPath: '/home',
                 scaffoldKey: _scaffoldKey,
-              )
+              ),
+              FutureBuilder(
+                future: Global.stickerRef.getData(),
+                builder: (context, AsyncSnapshot snap) {
+                  if (snap.hasData) {
+                    List<Sticker> stickers = snap.data;
+                    return Text('${stickers[0].tag}');
+                  }
+                  return Loader();
+                },
+              ),
+              // StreamBuilder(
+              //   stream: FirebaseFirestore.instance
+              //       .collection('stickers')
+              //       .snapshots(),
+              //   builder: (context, snapshot) {
+              //     if (snapshot.hasData) {
+              //       return Text(
+              //         '${snapshot.data.documents.map((doc) => "${doc['tag']}")}',
+              //       );
+              //     }
+              //     return Text('loading...');
+              //   },
+              // ),
             ],
           ),
         ),
