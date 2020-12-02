b666a171c90e311a76517bb2f5bfabeb9ce3497b
Akash Das <aakashdas368@gmail.com>
2020-12-02 19:28:38 +0530

    create a Global service with all immutable serivces

---
+++ lib/services/globals.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+import 'services.dart';
+
+/// Static global state. Immutable services that do not care about build context.
+class Global {
+  // App Data
+  static final String title = 'Steker';
+
+  // Services
+  static final FirebaseAnalytics analytics = FirebaseAnalytics();
+
+  // Data Models
+  static final Map models = {
+    Sticker: (data) => Sticker.fromJson(data),
+  };
+}
