5e835c73011b9364d338e5d885477c6402ceb2bf
Akash Das <aakashdas368@gmail.com>
2020-12-07 18:51:58 +0530

    add list itmes in drawer

--- lib/shared/drawer.dart
+++ lib/shared/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -14,20 +15,47 @@ class AppDrawer extends StatelessWidget {
             child: DrawerHeader(
               child: CircleAvatar(
                 backgroundColor: Theme.of(context).accentColor,
-                backgroundImage: NetworkImage(user.photoURL ?? ''),
+                backgroundImage: NetworkImage(
+                  user?.photoURL ??
+                      'https://images.unsplash.com/photo-1582266255765-fa5cf1a1d501?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+                ),
               ),
             ),
           ),
           Container(
             child: Column(
-              children: List.generate(4, (int index) {
-                return ListTile(
+              children: [
+                ListTile(
                   leading: Icon(
-                    Icons.info,
+                    FontAwesome.user_o,
                     color: Theme.of(context).textTheme.bodyText1.color,
                   ),
-                );
-              }),
+                  title: Text(
+                    user?.displayName ?? 'Guest',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    FontAwesome.exclamation_circle,
+                    color: Theme.of(context).textTheme.bodyText1.color,
+                  ),
+                  title: Text(
+                    'About',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    FontAwesome.code,
+                    color: Theme.of(context).textTheme.bodyText1.color,
+                  ),
+                  title: Text(
+                    'Code',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
