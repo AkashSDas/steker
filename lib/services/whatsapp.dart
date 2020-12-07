3d7eaf23cfa0fa963e301e0c9e97ddff0d14d45f
Akash Das <aakashdas368@gmail.com>
2020-12-07 20:33:25 +0530

    multiple sticker packs can be added but tray_img has to different

--- lib/services/whatsapp.dart
+++ lib/services/whatsapp.dart
@@ -7,30 +7,43 @@ import 'package:whatsapp_stickers/whatsapp_stickers.dart';
 
 Future installFromRemote(List<dynamic> stickers, String stickerTag) async {
   var applicationDocumentsDirectory = await getApplicationDocumentsDirectory();
-  var stickersDirectory =
-      Directory('${applicationDocumentsDirectory.path}/stickers');
-  await stickersDirectory.create(recursive: true);
+  var stickersDirectory = Directory(
+    '${applicationDocumentsDirectory.path}/stickers',
+  );
+
+  if (stickersDirectory.existsSync()) {
+    stickersDirectory.deleteSync(recursive: true);
+  }
+  stickersDirectory.createSync(recursive: true);
 
   final dio = Dio();
   final downloads = <Future>[];
+
+  Directory stickerPackDir = Directory('${stickersDirectory.path}/$stickerTag');
+
+  if (!stickerPackDir.existsSync()) {
+    stickerPackDir.createSync(recursive: true);
+  }
 
   stickers.asMap().forEach((index, stickerUrl) {
     downloads.add(
       dio.download(
         '$stickerUrl',
-        '${stickersDirectory.path}/$stickerTag-${index + 1}.webp',
+        '${stickersDirectory.path}/$stickerTag/$stickerTag-${index + 1}.webp',
       ),
     );
   });
 
   await Future.wait(downloads);
 
+  print(downloads);
+
   var stickerPack = WhatsappStickers(
     identifier: '$stickerTag',
     name: '$stickerTag stickers',
     publisher: 'AkashSDas',
     trayImageFileName: WhatsappStickerImage.fromAsset(
-      'assets/tray_img.png',
+      'assets/tray_img1.png',
     ),
     publisherWebsite: '',
     privacyPolicyWebsite: '',
@@ -40,11 +53,13 @@ Future installFromRemote(List<dynamic> stickers, String stickerTag) async {
   stickers.asMap().forEach((index, stickerUrl) {
     stickerPack.addSticker(
       WhatsappStickerImage.fromFile(
-        '${stickersDirectory.path}/$stickerTag-${index + 1}.webp',
+        '${stickersDirectory.path}/$stickerTag/$stickerTag-${index + 1}.webp',
       ),
       ['', ''],
     );
   });
+
+  print(stickerPackDir.listSync());
 
   try {
     await stickerPack.sendToWhatsApp();
