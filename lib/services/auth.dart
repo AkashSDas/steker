b0dea5901e63c9fced5cf2ceb7bbaf2d4c879cc7
Akash Das <aakashdas368@gmail.com>
2020-12-08 07:06:58 +0530

    rm redundant code from auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   // Firebase user one-time fetch
